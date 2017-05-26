7d30fab4ec88b7cc65f3c1fd2b274158ec0566ed
apfranzen <adamfranzen@gmail.com>
2017-05-26 10:48:12 -0400

    working version with 1 action

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+function reducer(state=0, action) {
+  if (action.type === 'INCREMENT') {
+    return ++state;
+  }
+  if (action.type === 'DECREMENT') {
+    return --state;
+  }
+  return state;
+}
+
+export default reducer;
